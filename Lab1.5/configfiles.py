7b77f305a3ed5d42fcca9b026d0b6cfa71d2f0a9
nojamrus <nojam@yandex.ru>
2017-06-09 16:26:30 +0300

    configfiles.py with re

--- Lab1.5/configfiles.py
+++ Lab1.5/configfiles.py
@@ -1,6 +1,8 @@
 import glob
+import re
 
 list_files=glob.glob(r"C:\t\Seafile\Seafile\p4ne_training\config_files\*.txt")
+ipPattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
 
 res_list=[]
 for one_file in list_files:
@@ -8,11 +10,15 @@ for one_file in list_files:
     lines = f.readlines()
     for one_line in lines:
         ret=str.find(one_line,'ip address ')
-        if ret!=-1 and ret==1:
-            one_line = str.strip(one_line)
-            res_list.append(one_line)
+        if ret!=-1:
+            findIP = re.findall(ipPattern, one_line)
+            if findIP:
+                one_line = str.strip(one_line)
+                res_list.append(one_line)
     f.close()
 
 unique_res=sorted(list(set(res_list)))
 for one_res in unique_res:
     print (one_res)
+
+
